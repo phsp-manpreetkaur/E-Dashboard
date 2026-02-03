614ca69d232550544e4ec1eb0523611f055be1dc
manpreetpahasp <preetkaur@pahasp.com>
2026-02-03 11:40:26 +0530

    commit

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,17 @@
 const express = require('express');
+const mongoose = require('mongoose');
 const app = express();
-app.get('/', (req, res) => {
-  res.send('Hello World!');
-});
-app.listen(3000, () => {
-  console.log('Server is running on http://localhost:3000');
-}); 
+
+const connectDB = async () => {
+  await mongoose.connect('mongodb://localhost:27017/e-commerce');
+
+  const productSchema = new mongoose.Schema({});
+  const Product = mongoose.model('Product', productSchema);
+
+  const data = await Product.find();
+  console.log(data);
+};
+
+connectDB();
+
+app.listen(5000);
