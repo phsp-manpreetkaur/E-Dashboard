9e215b945d3b324aef0148622d9a21531765e7c1
manpreetpahasp <preetkaur@pahasp.com>
2026-02-03 12:20:56 +0530

    Signup API

--- backend/index.js
+++ backend/index.js
@@ -1,12 +1,16 @@
 const express = require('express');
-// const mongoose = require('mongoose');
 require('./db/config');
 const users = require('./db/users');
-const app = express();
 
-app.post("/register", (req, res) => {
-  res.send("Api is working");
+const app = express();
+app.use(express.json());
+
+app.post("/register", async (req, res) => {
+  let user = new users(req.body);
+  let result = await user.save();
+  res.send(result);
 });
 
 
-app.listen(5000);
+
+app.listen(5000) ;
